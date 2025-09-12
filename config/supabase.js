b2f6040e063ab303814c9e9736a37b4ec3e3843d
lami216 <iiyad21694@gmail.com>
2025-09-12 21:47:57 +0000

    Remove authentication and account features

--- config/supabase.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://kllrcqcfrydllqxwudad.supabase.co";
-const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsbHJjcWNmcnlkbGxxeHd1ZGFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2NDAwMDksImV4cCI6MjA3MzIxNjAwOX0.Jisge_vucs0Xv-p4mVaU9uLWnU4oPpTGM1rU6cquV-E";
-
-window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true
-  }
-});
